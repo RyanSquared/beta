1f32b465761e5a0c28c67b5b2e361d4eb970656b
RyanSquared <vandor2012@gmail.com>
2020-03-19 04:05:17 -0500

    mediapanel_beta/content_manager/device_list: Initial Commit

--- mediapanel_beta/content_manager/__init__.py
+++ mediapanel_beta/content_manager/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 
 from .resources import ListResources, NewResource, Resource, ResourceFile
+from .device_list import ListDevices
 
 blueprint = Blueprint("content_manager", __name__, url_prefix="/manage")
 
@@ -10,8 +11,12 @@ blueprint.add_url_rule("/<_type>/<target_id>/resources",
                        view_func=ListResources.as_view("list_resources"))
 blueprint.add_url_rule("/<_type>/<target_id>/resources/new",
                        view_func=NewResource.as_view("upload_resource"))
-blueprint.add_url_rule("/<_type>/<target_id>/resources/manage/<resource_id>",
+blueprint.add_url_rule("/<_type>/<target_id>/resources/by-id/<resource_id>",
                        view_func=Resource.as_view("manage_resource"))
 blueprint.add_url_rule(
         "/<content_type>/<client_id>/file/<target_id>/<path:filename>",
         view_func=ResourceFile.as_view("resource_file"))
+
+# Device-specific Applications
+blueprint.add_url_rule("/device",
+                       view_func=ListDevices.as_view("list_devices"))
---
+++ mediapanel_beta/content_manager/device_list.py
@@ -0,0 +1,53 @@
+from datetime import datetime, timedelta
+
+from flask import abort, g, safe_join, request
+
+from ..app_view import AppRouteView, response
+from ..auth import login_required
+from ..models import Asset, db
+
+
+class ListDevices(AppRouteView):
+    # Allows for GET
+    # Identifiers: device
+    decorators = [login_required]
+    template_name = "content_manager/list_devices.html"
+
+    def populate(self):
+        device_list = []
+        query = request.args.get("search")
+
+        for device in g.user.allowed_devices:
+            if query is not None:
+                if query.lower() not in device.nickname.lower():
+                    continue
+            # Calculate human-readable offline time for device
+            now = datetime.now()
+            offline_delta = now - device.last_ping
+            if offline_delta.days > 30:  # x months
+                offline_for = str(int(offline_delta.days / 30)) + " months"
+            elif offline_delta.days > 0:  # x days
+                offline_for = str(int(offline_delta.days)) + " days"
+            elif offline_delta.seconds > (60 * 60):  # x hours
+                offline_for = str(int(offline_delta.seconds / (60 * 60))) + " hours"
+            else:  # x minutes
+                offline_for = str(int(offline_delta.seconds / 60)) + " minutes"
+
+            # Add device to list
+            device_list.append({
+                "device_id": device.device_id,
+                "last_ping": device.last_ping.timestamp(),
+                "nickname": device.nickname,
+                "system_version": device.system_version,
+                "device_ip": device.device_ip,
+                "total_disk": device.total_disk,
+                "free_disk": device.free_disk,
+                "offline_for": offline_for,
+                "is_offline": offline_delta.days > 0 or offline_delta.seconds > 90,
+            })
+
+        return {
+            "devices": device_list,
+            "current_time": datetime.now(),
+            "query": query,
+        }
